ed26e885a2daae4bc192d87887f2fdf2f70a4c15
RodrigoCesarAntunes <ro_sgca@hotmail.com>
2018-10-21 14:20:19 -0300

    docker removed

--- ApiCorePet/Startup.cs
+++ ApiCorePet/Startup.cs
@@ -34,7 +34,7 @@ namespace ApiCorePet
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Model.veterinarioserviceContext context)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -46,8 +46,7 @@ namespace ApiCorePet
             }
 
             app.UseHttpsRedirection();
-
-            context.Database.Migrate();
+            
             app.UseMvc();
         }
     }
